603a9ff191e7c968311accac1060db607708e214
weipeng <creatorkuang@gmail.com>
2015-12-07 15:33:49 +0800

    change version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'creatorkuang:react-meteor-status',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'react package for meteor status ',
   // URL to the Git repository containing the source code for this package.
